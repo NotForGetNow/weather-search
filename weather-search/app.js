59b145d012eee9129c7addbd70171a7953ddfbe9
ChillingTime <102903069+NotForGetNow@users.noreply.github.com>
2024-03-28 18:52:54 +0500

    Add files via upload

---
+++ weather-search/app.js
@@ -0,0 +1,17 @@
+const key = 'c0e8bde454822b66ca86dc975063465b'
+
+async function weather(city) {
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+    const response = await fetch(api)
+    const data = await response.json()
+    console.log(data)
+
+    document.querySelector('.country').textContent = `${data.name}`
+    document.querySelector('.deg').textContent = `${data.main.temp}°C`;
+    document.querySelector('.humi-h2').textContent = `${data.main.humidity}`
+    document.querySelector('.main').scr = `${data.weather.main}.png`
+}
+
+document.querySelector('.poisk').addEventListener('click', () => {
+    weather(document.querySelector('input').value)
+})
